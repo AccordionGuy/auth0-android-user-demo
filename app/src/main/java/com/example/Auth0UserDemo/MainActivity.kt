370d049cf1f9015a010d9564167aae7914403dd0
Joey deVilla <joey@joeydevilla.com>
2021-11-12 23:13:43 -0500

    Add basic template

---
+++ app/src/main/java/com/example/Auth0UserDemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.Auth0UserDemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
